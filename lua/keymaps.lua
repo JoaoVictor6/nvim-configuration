b2e5859e428884b98c6ef5eea668f207800fd129
João Victor <santosjoao.dev@gmail.com>
2024-09-09 20:55:18 -0300

    improve buffer keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,8 +2,8 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- barbar config! https://github.com/romgrk/barbar.nvim
-map('n', '<A-Tab>', '<Cmd>BufferNext<CR>', opts)
-map('n', '<A-S-Tab>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader>bn', '<Cmd>BufferNext<CR>', opts)
+map('n', '<leader>bb', '<Cmd>BufferPrevious<CR>', opts)
 
 
 -- nvim tree!
