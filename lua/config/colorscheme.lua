c8043a4b187ea52713b52ece6be91f6ff37829ae
João Candido <joao.candido@mutant.com.br>
2024-11-08 09:46:28 -0300

    current colorschme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,10 +1,9 @@
 
 -- colorschem
--- local ok, _ = pcall(vim.cmd, 'colorscheme gotham256')
--- if not ok then
---   vim.cmd 'colorscheme gotham356' 
--- end
+local ok, _ = pcall(vim.cmd, 'colorscheme gotham256')
+if not ok then
+  vim.cmd 'colorscheme gotham356' 
+end
 
 -- vim.cmd 'colorscheme catppuccin-latte'
 
-vim.cmd 'colorscheme dracula'
