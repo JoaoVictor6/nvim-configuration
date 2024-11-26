84370282f542dbfc64bf6f33a922a23db03063e8
João Victor <santosjoao.dev@gmail.com>
2024-11-25 21:43:19 -0300

    fix colorscheme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,7 +1,1 @@
-
--- colorschem
-local ok, _ = pcall(vim.cmd, 'colorscheme gotham256')
-if not ok then
-  vim.cmd 'colorscheme catppuccin-macchiato' 
-end
-
+vim.cmd 'colorscheme catppuccin-macchiato' 
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -74,6 +74,20 @@ require("lazy").setup({
       }
     },
     {
+      "goolord/alpha-nvim",
+      -- dependencies = { 'echasnovski/mini.icons' },
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+        local startify = require("alpha.themes.startify")
+        -- available: devicons, mini, default is mini
+        -- if provider not loaded and enabled is true, it will try to use another provider
+        startify.file_icons.provider = "devicons"
+        require("alpha").setup(
+          startify.config
+        )
+      end,
+    },
+    {
       "folke/trouble.nvim",
       opts = {}, -- for default options, refer to the configuration section for custom setup.
       cmd = "Trouble",
