45775e8a816b0931b35805065639c90e22df5fef
Joao Victor Dos Santos Candido <santosjoao.dev@gmail.com>
2025-10-05 15:48:33 -0300

    feat: add theme switch

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,9 +1,17 @@
-vim.cmd([[
-augroup TransparentBackground
-autocmd!
-autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
-autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
-augroup END
-]])
 
-vim.cmd([[colorscheme gruvbox]])
+local function switch_theme()
+  local current_theme = vim.g.colors_name
+
+  if current_theme == "gruvbox" then
+    vim.o.background = "light"
+    vim.cmd([[colorscheme catppuccin-latte]])
+    print("☀️ Tema trocado para Catppuccin Latte (light)")
+  else
+    -- Troca para o tema escuro
+    vim.o.background = "dark"
+    vim.cmd([[colorscheme gruvbox]])
+    print("🌙 Tema trocado para Gruvbox (dark)")
+  end
+end
+
+vim.api.nvim_create_user_command("SwitchTheme", switch_theme, {})
